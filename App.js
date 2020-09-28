82777218d1128a174f1e3a653164b0a29a07d327
seungchoi2019 <lioncsh200@gmail.com>
2020-09-27 22:06:14 -0500

    MapView Enabled

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, SafeAreaView, Dimensions } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <MapView
+        style={styles.mapContainer}
+        initialRegion={{
+          latitude: 42.047455,
+          longitude: -87.680657,
+          latitudeDelta: 0.05,
+          longitudeDelta: 0.05,
+        }}
+      >
+        <Marker coordinate={{ latitude: 42.035647, longitude: -87.669332 }} />
+      </MapView>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  mapContainer: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height * 0.8,
   },
 });

ff08f86465d6f663b2f0a4a6fba79cf74aa3e6ae
Daniella <daniellaa00@gmail.com>
2020-09-30 14:37:57 -0500

    Search Bar Update

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import React, {Component} from "react";
 import { View, StyleSheet, Text, SafeAreaView, Dimensions, Image } from "react-native";
 import MapView, { Marker, Callout } from "react-native-maps";
 import { WebView } from "react-native-webview";
-import { Searchbar } from "react-native-paper";
+import { SearchBar } from "react-native-elements";
 
 
 export default function App() {
@@ -13,43 +13,46 @@ export default function App() {
 
   const onChangeSearch = query => {
 	  setSearchQuery(query);
-		
-  }
-  
-  const onSearchButtonPress = () => {
-	  setSearchQuery("I have searched");
-  }
-  return (
- 
-   <SafeAreaView style={styles.container}>
-	  <Searchbar
-        placeholder="Search"
-        onChangeText={onChangeSearch}
-        value={searchQuery}
-		style={styles.searchBar}
-		onSubmitEditing={onSearchButtonPress}
-      />
-      <MapView
-        style={styles.mapContainer}
-        initialRegion={{
-          latitude: 42.047455,
-          longitude: -87.680657,
-          latitudeDelta: 0.05,
-          longitudeDelta: 0.05,
-        }}
-      >
-        <Marker coordinate={{ latitude: 42.035647, longitude: -87.669332 }}>
-          <Callout>
-            <CustomCalloutView />
-          </Callout>
-        </Marker>
-      </MapView>
-      <StatusBar style="auto" />
 
-	   <Text>
-	  {searchQuery}
+  }
+  const onSearchButtonPress = () => {
+    setSearchQuery("works!");
+  }
+
+  return (
+    <View style={styles.container}>
+      <SafeAreaView>
+        <SearchBar
+          style={styles.searchBar}
+          platform="default"
+          placeholder="Search"
+          onChangeText={onChangeSearch}
+          value={searchQuery}
+          onSubmitEditing={onSearchButtonPress}
+          lightTheme="true"
+        />
+        <MapView
+          style={styles.mapContainer}
+          initialRegion={{
+            latitude: 42.047455,
+            longitude: -87.680657,
+            latitudeDelta: 0.05,
+            longitudeDelta: 0.05,
+          }}
+        >
+          <Marker coordinate={{ latitude: 42.035647, longitude: -87.669332 }}>
+            <Callout>
+              <CustomCalloutView />
+            </Callout>
+          </Marker>
+      </MapView>
+      <StatusBar style="light-content" />
+	   <Text style={styles.results}>
+	    {searchQuery}
 	  </Text>
+
     </SafeAreaView>
+    </View>
   );
 }
 
@@ -84,11 +87,7 @@ const styles = StyleSheet.create({
     width: Dimensions.get("window").width,
     height: Dimensions.get("window").height * 0.8,
   },
-  searchBar: {
-	flex: 0,
-	position: 'absolute',
-	top: 20,
-	left: 0,
-	right: 0,	
-  }
+  results: {
+    marginTop: '2%',
+  },
 });

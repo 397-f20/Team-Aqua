ac80fab6b62df91318ae2bb2f872f74d59dd864e
seungchoi2019 <lioncsh200@gmail.com>
2020-09-30 23:39:55 -0500

    Modal upgrade with dummy items for now

--- App.js
+++ App.js
@@ -1,26 +1,33 @@
 import { StatusBar } from "expo-status-bar";
-import React, {Component} from "react";
-import { View, StyleSheet, Text, SafeAreaView, Dimensions, Image, Modal, TouchableHighlight } from "react-native";
+import React, { Component, useState } from "react";
+import {
+  View,
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  Dimensions,
+  Image,
+  Modal,
+  TouchableHighlight,
+} from "react-native";
 import MapView, { Marker, Callout } from "react-native-maps";
 import { WebView } from "react-native-webview";
 import { SearchBar } from "react-native-elements";
 import ModalPopUp from "./components/ModalPopUp";
-
+import dummy_pins from "./dummy_pins.json";
 
 export default function App() {
+  var text = "";
+  const [searchQuery, setSearchQuery] = useState("");
+  const [modalVisible, setModalVisible] = useState(false);
+  const [pinSelected, setPinSelected] = useState(0);
 
-	var text = '';
-  const [searchQuery, setSearchQuery] = React.useState('');
-  const [modalVisible, setModalVisible] = React.useState(false);
-
-
-  const onChangeSearch = query => {
-	  setSearchQuery(query);
-
-  }
+  const onChangeSearch = (query) => {
+    setSearchQuery(query);
+  };
   const onSearchButtonPress = () => {
     setSearchQuery("works!");
-  }
+  };
 
   return (
     <View style={styles.container}>
@@ -34,16 +41,6 @@ export default function App() {
           onSubmitEditing={onSearchButtonPress}
           lightTheme={true}
         />
-
-      <TouchableHighlight
-        style={styles.openButton}
-        onPress={() => {
-          setModalVisible(!modalVisible);
-        }}
-      >
-        <Text style={styles.textStyle}>Show Modal</Text>
-      </TouchableHighlight>
-
         <MapView
           style={styles.mapContainer}
           initialRegion={{
@@ -52,46 +49,58 @@ export default function App() {
             latitudeDelta: 0.05,
             longitudeDelta: 0.05,
           }}
-
         >
-          <Marker
-          coordinate={{ latitude: 42.035647, longitude: -87.669332 }}
-          onPress={() => {
-            setModalVisible(!modalVisible);
-          }}
+          {dummy_pins["markers"].map((pin) => (
+            <Marker
+              coordinate={{
+                latitude: +pin.latitude,
+                longitude: +pin.longitude,
+              }}
+              onPress={() => {
+                setModalVisible(!modalVisible);
+                setPinSelected(pin.id);
+              }}
+              identifier={pin.id}
+            />
+          ))}
+        </MapView>
+        {modalVisible ? (
+          <ModalPopUp
+            modalVisible={modalVisible}
+            setModalVisible={setModalVisible}
+            pinId={pinSelected}
           />
-      </MapView>
-			{modalVisible? <ModalPopUp state={true}/> : null}
-
-      <StatusBar style="light-content" />
-	   <Text style={styles.results}>
-	    {searchQuery}
-	  </Text>
-
-    </SafeAreaView>
+        ) : null}
+        <StatusBar style="light-content" />
+        <Text style={styles.results}>{searchQuery}</Text>
+      </SafeAreaView>
     </View>
   );
 }
 
-class CustomCalloutView extends React.Component {
-    render() {
-        return (
-            <View>
-                <View>
-                  <Text style={{
-                      fontWeight: "bold",
-                  }}>
-					Test
-                </Text>
-                </View>
-                <View>
-                  <WebView style={{ height: 100 , width: 100, }} source={{ uri: 'https://facebook.github.io/react/logo-og.png' }} />
-                </View>
-            </View>
-
-        )
-    }
-}
+// class CustomCalloutView extends React.Component {
+//   render() {
+//     return (
+//       <View>
+//         <View>
+//           <Text
+//             style={{
+//               fontWeight: "bold",
+//             }}
+//           >
+//             Test
+//           </Text>
+//         </View>
+//         <View>
+//           <WebView
+//             style={{ height: 100, width: 100 }}
+//             source={{ uri: "https://facebook.github.io/react/logo-og.png" }}
+//           />
+//         </View>
+//       </View>
+//     );
+//   }
+// }
 
 const styles = StyleSheet.create({
   container: {
@@ -104,14 +113,18 @@ const styles = StyleSheet.create({
     width: Dimensions.get("window").width,
     height: Dimensions.get("window").height * 0.8,
   },
+  mapContainerModal: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height * 0.4,
+  },
   results: {
-    marginTop: '2%',
+    marginTop: "2%",
   },
   centeredView: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    marginTop: 22
+    marginTop: 22,
   },
   modalView: {
     backgroundColor: "white",
@@ -125,25 +138,25 @@ const styles = StyleSheet.create({
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
-      height: 2
+      height: 2,
     },
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
-    elevation: 5
+    elevation: 5,
   },
   openButton: {
     backgroundColor: "#F194FF",
     borderRadius: 20,
     padding: 10,
-    elevation: 2
+    elevation: 2,
   },
   textStyle: {
     color: "white",
     fontWeight: "bold",
-    textAlign: "center"
+    textAlign: "center",
   },
   modalText: {
     marginBottom: 15,
-    textAlign: "center"
-  }
+    textAlign: "center",
+  },
 });
--- components/ModalPopUp.js
+++ components/ModalPopUp.js
@@ -1,9 +1,22 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, Modal, TouchableHighlight, Dimensions } from "react-native";
-import { WebView } from "react-native-webview";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Modal,
+  TouchableHighlight,
+  Dimensions,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import dummy_pins from "../dummy_pins.json";
+import WebView from "react-native-webview";
 
-const ModalPopUp =  ({state}) => {
-  const [modalVisible, setModalVisible] = React.useState(state);
+const ModalPopUp = ({ modalVisible, setModalVisible, pinId }) => {
+  const currentPin = dummy_pins["markers"].filter(function (item) {
+    return item.id === pinId;
+  });
+
   return (
     <Modal
       animationType="slide"
@@ -14,30 +27,37 @@ const ModalPopUp =  ({state}) => {
         Alert.alert("Modal has been closed.");
       }}
     >
+      <TouchableOpacity
+        style={styles.container}
+        activeOpacity={1}
+        onPress={() => {
+          setModalVisible(false);
+        }}
+      />
       <View style={styles.centeredView}>
         <View style={styles.modalView}>
-          <Text style={styles.modalText}>Hello World!</Text>
-
-          <TouchableHighlight
-            style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
-            onPress={() => {
-              setModalVisible(!modalVisible);
-            }}
-          >
-            <Text style={styles.textStyle}>Hide Modal</Text>
-          </TouchableHighlight>
+          <WebView
+            style={styles.image}
+            source={{ uri: `${currentPin[0].uri}` }}
+          />
+          <Text style={styles.modalText}>{currentPin[0].title}</Text>
+          <Text style={styles.modalText}>{currentPin[0].description}</Text>
         </View>
       </View>
     </Modal>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
+  image: {
+    width: Dimensions.get("window").width,
+    height: 400,
+  },
   centeredView: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    marginTop: 22
+    marginTop: 22,
   },
   modalView: {
     backgroundColor: "white",
@@ -51,21 +71,26 @@ const styles = StyleSheet.create({
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
-      height: 2
+      height: 2,
     },
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
-    elevation: 5
+    elevation: 5,
   },
-    textStyle: {
-      color: "white",
-      fontWeight: "bold",
-      textAlign: "center"
-    },
-    modalText: {
-      marginBottom: 15,
-      textAlign: "center"
-    },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
+  },
+  container: {
+    flex: 1,
+    height: Dimensions.get("window").height * 0.5,
+    top: 0,
+  },
 });
 
 export default ModalPopUp;

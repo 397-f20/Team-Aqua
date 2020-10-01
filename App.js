f23319622795e64fad62ae09084d16c07fc1d723
ntantwan <ntantwan@gmail.com>
2020-09-30 19:12:54 -0500

    modal half-screen. still need to make a new map style that resizes map and toggles when this is opened

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import React, {Component} from "react";
-import { View, StyleSheet, Text, SafeAreaView, Dimensions, Image } from "react-native";
+import { View, StyleSheet, Text, SafeAreaView, Dimensions, Image, Modal, TouchableHighlight } from "react-native";
 import MapView, { Marker, Callout } from "react-native-maps";
 import { WebView } from "react-native-webview";
 import { SearchBar } from "react-native-elements";
@@ -10,6 +10,8 @@ export default function App() {
 
 	var text = '';
   const [searchQuery, setSearchQuery] = React.useState('');
+  const [modalVisible, setModalVisible] = React.useState(false);
+
 
   const onChangeSearch = query => {
 	  setSearchQuery(query);
@@ -31,6 +33,42 @@ export default function App() {
           onSubmitEditing={onSearchButtonPress}
           lightTheme="true"
         />
+        
+        <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        //presentationStyle = {"fullScreen"}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Hello World!</Text>
+
+            <TouchableHighlight
+              style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
+              onPress={() => {
+                setModalVisible(!modalVisible);
+              }}
+            >
+              <Text style={styles.textStyle}>Hide Modal</Text>
+            </TouchableHighlight>
+          </View>
+        </View>
+      </Modal>
+
+      <TouchableHighlight
+        style={styles.openButton}
+        onPress={() => {
+          setModalVisible(true);
+        }}
+      >
+        <Text style={styles.textStyle}>Show Modal</Text>
+      </TouchableHighlight>
+
+
         <MapView
           style={styles.mapContainer}
           initialRegion={{
@@ -39,8 +77,14 @@ export default function App() {
             latitudeDelta: 0.05,
             longitudeDelta: 0.05,
           }}
+          
         >
-          <Marker coordinate={{ latitude: 42.035647, longitude: -87.669332 }}>
+          <Marker
+          coordinate={{ latitude: 42.035647, longitude: -87.669332 }}
+          onPress={() => {
+            setModalVisible(true);
+          }}
+          >
             <Callout>
               <CustomCalloutView />
             </Callout>
@@ -90,4 +134,43 @@ const styles = StyleSheet.create({
   results: {
     marginTop: '2%',
   },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    backgroundColor: "white",
+    position: "absolute",
+    bottom: 0,
+    borderRadius: 20,
+    padding: 55,
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height * 0.5,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5
+  },
+  openButton: {
+    backgroundColor: "#F194FF",
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
+  }
 });

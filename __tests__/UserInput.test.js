a552ecceee8f67ea459e807872682e625dcb5fc2
Riley <rag16c@my.fsu.edu>
2020-11-02 00:56:33 -0500

    Added Ciprus, clustering on mobile

---
+++ __tests__/UserInput.test.js
@@ -0,0 +1,25 @@
+import 'react-native'
+import React from 'react'
+import {fireEvent, render, waitFor} from 'react-native-testing-library'
+import {expect, it} from '@jest/globals'
+import UserInput from '../components/UserInput'
+
+//the modal component is automatically mocked by RN and apparently contains a bug which make the modal (and it's children) always visible in the test tree
+//this is a hack which fix this issue
+jest.mock('react-native/Libraries/Modal/Modal', () => {
+  const Modal = jest.requireActual('react-native/Libraries/Modal/Modal');
+// @ts-ignore
+  return props=> <Modal {...props}/>
+})
+
+it('renders input modal correctly', async () => {
+  const {getByText, getByTestId, debug} = render(<UserInput />)
+
+  expect(()=> getByText(/Add Pin/i)).toThrow(/no instances found/i) //modal is initially closed
+
+  fireEvent.press(getByText(/Add a Spot/i))
+  await waitFor(()=> getByText(/Add Pin/i)) //modal is now visible
+
+  fireEvent.press(getByTestId('close'))
+  expect(()=> getByText(/Add Pin/i)).toThrow(/no instances found/i) //modal is closed again
+});

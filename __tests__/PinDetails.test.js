9fa8d199b52fec6278080f0a234605735d071245
seungchoi2019 <lioncsh200@gmail.com>
2020-11-04 16:42:48 -0600

    Github stashed half of my new test codes

--- __tests__/PinDetails.test.js
+++ __tests__/PinDetails.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import renderer from "react-test-renderer";
+import renderer from "react-test renderer";
 
 import PinDetails from "../components/PinDetails";
 import PinList from "../components/PinList";
@@ -10,11 +10,6 @@ import { expect, it } from "@jest/globals";
 jest.mock("react-native/Libraries/Modal/Modal", () => {
   const Modal = jest.requireActual("react-native/Libraries/Modal/Modal");
   return (props) => <Modal {...props} />;
-<<<<<<< Updated upstream
-})
-  it('shows pin details correctly', async () => {
-  });
-=======
 });
 
 describe("Fetches the right pin details from database", () => {
@@ -39,6 +34,21 @@ describe("Fetches the right pin details from database", () => {
   });
 
   // PinList can also call PinDetails when user presses on one of the search result
-  it("Shows one PinDetail from PinList", async () => {});
+  it("Shows one PinDetail from PinList", async () => {
+    const pin = [
+      {
+        description: "Awesome place!!",
+        id: "1t35326",
+        latitude: "42.0559",
+        longitude: "-87.6702",
+        title: "Northwestern Lakefill",
+        uri:
+          "https://i.pinimg.com/originals/3b/17/41/3b17413b18a807023c02a9b674bb5756.jpg",
+      },
+    ];
+
+    const { getByText, getByTestId, debug } = render(<PinDetails pin={pin} />);
+    expect(() => getByText(/Northwestern Lakefill/i)).toBeTruthy();
+    expect(() => getByText(/Find Directions/i)).toBeTruthy();
+  });
 });
->>>>>>> Stashed changes

8fdb55fc267a102d7a0c237866807c576998ca00
seungchoi2019 <lioncsh200@gmail.com>
2020-11-04 21:23:23 -0600

    Fixed one of the import error and Jest now passed locally

--- __tests__/PinDetails.test.js
+++ __tests__/PinDetails.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import renderer from "react-test renderer";
+import renderer from "react-test-renderer";
 
 import PinDetails from "../components/PinDetails";
 import PinList from "../components/PinList";
